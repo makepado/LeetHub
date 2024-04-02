a0f63d6a78dcd779f713884d11bb7cc6eac722c2
Jaehyun Hong (Bobby) <76645966+makepado@users.noreply.github.com>
2024-04-02 17:04:42 +0900

    Time: 18 ms (6.89%), Space: 44.8 MB (6.35%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        String reverseStr = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {            
+            reverseStr = reverseStr + str.charAt(i);
+        }
+
+        return str.equals(reverseStr);
+    }
+}
