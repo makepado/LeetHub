d229d3e37a3298c9b91427c2b2a5762fbb8b6c07
Jaehyun Hong (Bobby) <76645966+makepado@users.noreply.github.com>
2024-04-22 16:43:20 +0900

    Time: 2 ms (97.65%), Space: 44.6 MB (81.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,11 +2,15 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
+        //순회
         for(int i=0; i < nums.length; i++) {
+            
+            //2. target - 현재값이 map에 있으면
             if(map.containsKey(target - nums[i])) {
+                //현재 index와 target-현재값의 index 리턴
                 return new int[] {i, map.get(target - nums[i])};
             }
-
+            //1. target - 현재값이 map에 없으면 key:현재값, value:index 추가
             map.put(nums[i], i);
         }
 
