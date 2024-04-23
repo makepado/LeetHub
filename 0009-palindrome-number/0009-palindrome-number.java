bf95c6e94b5a80b892c31f91ce10c64480af308a
Jaehyun Hong (Bobby) <76645966+makepado@users.noreply.github.com>
2024-04-23 11:34:26 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String str = String.valueOf(x);
-        String reverse = "";
+        
+        String buffer = new StringBuffer(str);
+        buffer.reverse();
 
-        for(int i=str.length() - 1; i >= 0; i--) {
-            reverse = reverse + str.charAt(i);
-        }
-
-        return str.equals(reverse);
+        return str.equals(buffer);
     }
 }
