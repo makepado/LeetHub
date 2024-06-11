66a1b44b6993123485a84cf1ee90faf4577ab45b
Jaehyun Hong (Bobby) <76645966+makepado@users.noreply.github.com>
2024-06-11 11:01:24 +0900

    Time: 2 ms (97.73%), Space: 44.5 MB (89.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,24 +2,21 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         //MAP을 이용해 key:현재값, value:index 로 구분
         HashMap<Integer, Integer> map = new HashMap<>();
-        int[] result = new int[2];
 
         //순회
         for(int i=0; i < nums.length; i++) {
-            
+            int currentValue = target - nums[i];
             //1. target - 현재값이 map에 있으면
-            if(map.containsKey(target - nums[i])) {
-
+            if(map.containsKey(currentValue)) {
+                int completeValue = map.get(currentValue);
                 //현재 index와 target-현재값의 index 리턴
-                result[0] = i;
-                result[1] = map.get(target - nums[i]);
-                return result;
+                return new int[] { i, completeValue };
             }
 
             //2. target - 현재값이 map에 없으면 추가
             map.put(nums[i], i);
         }
         //이외에 빈값을 리턴
-        return result;
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
